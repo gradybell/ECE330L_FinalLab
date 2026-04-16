a76ab93050d9b8ee1ad1053a9ec3659a050b0d46
gradybell <gradybell@u.boisestate.edu>
2026-04-15 23:42:58 -0600

    Update battleship.c

--- battleship.c
+++ battleship.c
@@ -1,70 +1,135 @@
 #include "battleship.h"
 
-// data
-int switches_in = 0;
-int leds_out = 0;
+//////////////////////////////////
+////////////// Data //////////////
+//////////////////////////////////
 
+/* Input variables */
+int switches_in = 0; // slider switch (PC0) used to toggle cursor between horizontal (0) and vertical (1)
+int pushButton_in = 0; // pushbutton (PC10) used to execute action (place boat & fire)
+int pot_in = 0; // potentiometer (PA1) used to control cursor
 
-static int timer = 0;
+/* Output variables */
+/*****************************/
+/*** TODO output variables ***/
+/*****************************/
 
-// function definitions
-int input( void )
-{
+/* Enumerated variables */
+typedef enum gameState {title = 0, playerOneStart = 1, playerTwoStart = 2,
+						playerOneTurn = 3, playerTwoTurn = 4, endGame = 5} gameState; // represents all states the game can be in
 
-	switches_in = GPIOC->IDR;
+typedef enum player {playerOne = 0, playerTwo = 1} player;
+
+typedef enum LEDstate {off = 0, dim = 1, bright = 2, blink = 3} LEDstate;
+
+static int timer = 0; // used to control game timing
+
+/* Data structure */
+typedef struct bitMap {
+	LEDstate horizontal_bitMap[3][8]; // 2D array of integers corresponding to the horizontal LEDs on the 7SEG display
+	LEDstate vertical_bitMap[2][16]; // 2D array of integers corresponding to the vertical LEDs on the 7SEG display
+} bitMap;
+
+/* Instances of maps */
+bitMap playerOneShipBoard, playerOneTargetBoard, playerTwoShipBoard, playerTwoTargetBoard;
+
+//////////////////////////////////
+////// Function Definitions //////
+//////////////////////////////////
+
+/**
+ * Process inputs from switches, pushbuttons, and potentiometer
+ */
+int input(void) {
+
+	switches_in = GPIOC->IDR & 1; // parse only the rightmost switch (PC0)
+	pushButton_in = (GPIOC->IDR >> 10) & 1; // parse only the switch connected to PC10
+
+	/* read potentiometer  */
+		HAL_Delay(1);
+		/*
+		 * Start a conversion on ADC1 by forcing bit 30 in CR2 to
+		 * 1 while keeping other bits unchanged
+		 */
+		ADC1->CR2 |= 1<<30;
+		HAL_Delay(1);
+		if (ADC1->SR & 1<<1) { // check for conversion completed
+			pot_in = (ADC1->DR);
+		}
 
 	return 0;
 }
 
-int logic ( void )
-{
+/**
+ * Game logic that reads inputs and produces outputs
+ */
+int logic (void) {
 
 	// process raw inputs
-//	leds_out = ~switches_in;
-	leds_out = timer >> 9;
 
 	// game logic
-	  for (int i=0 ; i<8 ; i++)      // Clear the display
-	  {
-		  Seven_Segment_Digit(i,SPACE,0);
-	  }
+	switch (gameState) { // game state dependent logic
 
-	  if ( timer % 5 == 0 ){
-		  Seven_Segment_Digit(7,CHAR_A,0);
-	  }
+		case title:
 
-	  if ( (timer / 500) % 2 == 0 ){
-		  Seven_Segment_Digit(6,CHAR_A,0);
-	  }
-	  Seven_Segment_Digit(5,CHAR_A,0);
+			break;
+
+		case playerOneStart:
+
+			break;
+
+		case playerTwoStart:
+
+			break;
+
+		case playerOneTurn:
+
+			break;
+
+		case playerTwoTurn:
+
+			break;
+
+		case endGame:
+
+			break;
+	}
 
 	// prepare outputs
-	// ...
 
 	return 0;
 }
 
-int output ( void )
-{
+/**
+ * draws board to 7SEG display
+ */
+int output (void) {
 
-	GPIOD->ODR = leds_out << 1;
+	GPIOD->ODR = pot_in; // TODO delete this (for testing only)
+
 	return 0;
 }
 
-int game( void )
-{
+int handle_interrupts (void) {
 
-	while(1)
-	{
+	timer++;
+
+	return 0;
+}
+
+int game(void) {
+
+	while(1) {
 		  input();
 		  logic();
 		  output();
 	}
 }
 
-int handle_interrupts ( void )
-{
+/////////////////////////////////
+//////////// Helpers ////////////
+/////////////////////////////////
 
+void drawBoard(struct bitMap) {
 
-	timer++;
 }
